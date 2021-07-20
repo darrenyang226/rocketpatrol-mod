0b928ef8a8ae196648347b832511e002e971ffba
Darren Yang <yyang199@ucsc.edu>
2021-07-19 21:15:06 -0700

    added credits

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ let game = new Phaser.Game(config);
 
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
-let keyS, keyR, keyLEFT, keyRIGHT, keyCLICK, input, mouse; //F
+let keyS, keyR, keyLEFT, keyRIGHT, keyCLICK, input, mouse; 
 
 
 //Name: Darren Yang, Project title: Rocket Patrol Modified, 7/07/2021, took estimated 4 days to complete.
@@ -21,5 +21,6 @@ let keyS, keyR, keyLEFT, keyRIGHT, keyCLICK, input, mouse; //F
 //Implement an alternating two-player mode (20),
 //Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20)  
 //Implement mouse control for player movement and mouse click to fire (20)
-
+// Credits: James Liu , https://www.youtube.com/watch?v=laNi0fdF_DU (two-player mode ), 
+//https://www.youtube.com/watch?v=88DS3Z8nOdY (Score, general coding)
 
